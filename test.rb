3acfa5b1227e8de02539098aa859b893f1bd3d51
MPelyhes <mattpelyhes@gmail.com>
2021-05-31 18:41:31 -0500

    add basic number excercises

--- test.rb
+++ test.rb
@@ -1,4 +1,4 @@
-# puts "Hello, World! I did it!"
+# number excercises
 
 def add(a, b)
    puts a + b
@@ -19,7 +19,25 @@ end
 remainder 20, 5
 
 def float_division(a,b)
-  puts a/b.to_i
+  puts a/b.to_f
 end
 
 float_division 6, 4
+
+def string_to_number(string)
+   puts string.to_i
+end
+
+string_to_number "86"
+
+def even?(number)
+   puts number.even?
+end
+
+even? 18
+
+def odd?(number)
+   puts number.odd?
+end
+
+odd? 11
