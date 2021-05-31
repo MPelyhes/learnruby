e5f6268c946ec3c12b1170883ee822657afcb058
MPelyhes <mattpelyhes@gmail.com>
2021-05-31 18:22:47 -0500

    first

---
+++ test.rb
@@ -0,0 +1,19 @@
+# puts "Hello, World! I did it!"
+
+def add(a, b)
+   puts a + b
+end
+
+# add 1, 3
+
+def subtract(a, b)
+   puts a - b
+end
+
+# subtract 5,3
+
+def remainder(a, b)
+   puts a % b
+end
+
+remainder 20, 5
